dc047f7a57b9585d440771a0be9443284fe8bec5
Indira117 <indirazhana@gmail.com>
2021-03-10 18:29:37 -0500

    15

--- src/com/collection.java
+++ src/com/collection.java
@@ -3,7 +3,7 @@ package com;
 public class collection {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 11; i++) {
+        for (int i = 0; i < 15; i++) {
             System.out.println("hello");
         }
     }
