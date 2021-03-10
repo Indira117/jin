5d4c4d8921f063b49bcc96da13fbe020c94b658e
Indira117 <indirazhana@gmail.com>
2021-03-10 18:26:17 -0500

    11111

---
+++ src/com/collection.java
@@ -0,0 +1,10 @@
+package com;
+
+public class collection {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 11; i++) {
+            System.out.println("hello");
+        }
+    }
+}
