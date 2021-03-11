d83d9ed81147f9a171d77c297b7d514f80db8a20
Indira117 <indirazhana@gmail.com>
2021-03-11 18:37:22 -0500

    1545

--- src/com/collection.java
+++ src/com/collection.java
@@ -1,10 +1,11 @@
 package com;
 
-public class collection {
+public class collection  {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
         for (int i = 0; i < 15; i++) {
-            System.out.println("hello");
+            System.out.println("hello);
+            Thread.sleep(2000);
         }
     }
 }
